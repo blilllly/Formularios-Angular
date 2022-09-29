d18dc679d19a1f531723cb56150faf8bdefb8ded
blilllly <alvearbilly@gmail.com>
2022-09-28 22:16:32 -0500

    reactive-dinamics

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  
+  constructor( private fb: FormBuilder) { }
+  
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [ '', [Validators.required, Validators.minLength(3) ] ],
+    favoritos: this.fb.array( [
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required]
+    ], Validators.required )
+  });
 
-  ngOnInit(): void {
+  nuevoFavorito: FormControl = this.fb.control( '', Validators.required );
+
+  get favoritosArr() {
+    return this.miFormulario.controls['favoritos'] as  FormArray;
+  }
+
+  guardar() {
+
+    if(this.miFormulario.invalid) { return }
+
+    console.log(this.miFormulario.value)
+    //imprimir solo si es valido
+  }
+
+  esInvalido( campo: string ) {
+    return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched
+  }
+
+  agregarFavorito() {
+    if(this.nuevoFavorito.invalid) { return; }
+
+    this.favoritosArr.push( this.fb.control( this.nuevoFavorito.value, Validators.required ) );
+
+    this.nuevoFavorito.reset();
+  }
+
+  borrar( i: number ) {
+    this.favoritosArr.removeAt(i);
   }
 
 }
